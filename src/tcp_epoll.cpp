794e3d824cf93e56454abcdd96494a6147a5118d
0WAQ <2758282037@qq.com>
2024-06-27 15:08:37 +0800

    重构,将新连接事件放到read events中

--- src/tcp_epoll.cpp
+++ src/tcp_epoll.cpp
@@ -65,82 +65,95 @@ int main(int argc, char* argv[])
     {
         int num_fds = epoll_wait(epoll_fd, evs, 10, -1);
         
-        if(num_fds < 0) { // error
+        // error
+        if(num_fds < 0) 
+        { 
             std::cerr << "epoll_wait() failed\n";
             break;
         }
-        else if(num_fds == 0) { // timeout
+        // timeout
+        else if(num_fds == 0) 
+        { 
             std::cout << "epoll_wait() timeout.\n";
             continue;
         }
         
         for(int i = 0; i < num_fds; i++) 
-        {   
-            // new connect
-            if(evs[i].data.fd == listen_fd)
-            {
-                ///////////////////////////////////////////
-                sockaddr_in clnt_addr;
-                socklen_t clnt_addr_len = sizeof(clnt_addr);
-
-                int clnt_fd = accept4(listen_fd, (sockaddr*)&clnt_addr, &clnt_addr_len, SOCK_NONBLOCK);
-
-                printf("Accept client(fd = %d, ip = %s, port = %d) ok.\n", 
-                            clnt_fd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
-
-                
-                // add clnt_fd to epoll_fd binds ev
-                ev.data.fd = clnt_fd;
-                ev.events = EPOLLIN | EPOLLET; // Edge Triggle
-                epoll_ctl(epoll_fd, EPOLL_CTL_ADD, clnt_fd, &ev);
-                ///////////////////////////////////////////
+        {
+            // close events
+            if(evs[i].events & EPOLLRDHUP) { 
+                printf("1.client(clnt_fd = %d) disconnected\n", evs[i].data.fd);
+                close(evs[i].data.fd);
             }
-            else
+            // read events
+            else if(evs[i].events & (EPOLLIN | EPOLLPRI)) 
             {
-                ///////////////////////////////////////////
-                if(evs[i].events & EPOLLRDHUP) { // close events
-                    printf("1.client(clnt_fd = %d) disconnected\n", evs[i].data.fd);
-                    close(evs[i].data.fd);
+                // listen_fd
+                if(evs[i].data.fd == listen_fd)
+                {
+                    ///////////////////////////////////////////
+                    sockaddr_in clnt_addr;
+                    socklen_t clnt_addr_len = sizeof(clnt_addr);
+
+                    int clnt_fd = accept4(listen_fd, (sockaddr*)&clnt_addr, &clnt_addr_len, SOCK_NONBLOCK);
+
+                    printf("Accept client(fd = %d, ip = %s, port = %d) ok.\n", 
+                                clnt_fd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
+
+                    
+                    // add clnt_fd to epoll_fd binds ev
+                    ev.data.fd = clnt_fd;
+                    ev.events = EPOLLIN | EPOLLET; // Edge Triggle
+                    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, clnt_fd, &ev);
+                    ///////////////////////////////////////////
                 }
-                else if(evs[i].events & (EPOLLIN | EPOLLPRI)) // read events
+                // other fd
+                else
                 {
                     char buf[1024];
-
                     while(true) // non-blocking IO
                     {
                         // init all buf as 0
                         bzero(&buf, sizeof(buf));
 
-                        ssize_t nread = read(evs[i].data.fd, buf, sizeof(buf));
+                        ssize_t nlen = read(evs[i].data.fd, buf, sizeof(buf));
+
                         // read datas successfully
-                        if(nread > 0) {
+                        if(nlen > 0) 
+                        {
                             printf("recv(clnt_fd = %d): %s\n", evs[i].data.fd, buf);
                             send(evs[i].data.fd, buf, strlen(buf), 0);
                         }
                         // read failed because interrupted by system call
-                        else if(nread == -1 && errno == EINTR) {
+                        else if(nlen == -1 && errno == EINTR) 
+                        {
                             continue;
                         }
                         // read failed because datas've been read
-                        else if(nread == -1 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+                        else if(nlen == -1 && (errno == EAGAIN || errno == EWOULDBLOCK)) 
+                        {
                             break;
                         }
                         // clnt has been disconnected
-                        else if(nread == 0) {
+                        else if(nlen == 0) 
+                        {
                             printf("2.client(clnt_fd = %d) disconnected.\n", evs[i].data.fd);
                             close(evs[i].data.fd);
                             break;
                         }
                     }
                 }
-                else if(evs[i].events & EPOLLOUT) { // write events
-                    
-                }
-                else {
-                    printf("client(clnt_fd = %d) error.\n", evs[i].data.fd);
-                    close(evs[i].data.fd);
-                }
-                ///////////////////////////////////////////
+            }
+            // write events
+            else if(evs[i].events & EPOLLOUT) 
+            { 
+                
+            }
+            // error events
+            else 
+            {
+                printf("client(clnt_fd = %d) error.\n", evs[i].data.fd);
+                close(evs[i].data.fd);
             }
         }
     }
