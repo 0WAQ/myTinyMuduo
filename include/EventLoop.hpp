2b44e608ef4852bd323403ce61be437cfc00a8ff
0WAQ <2758282037@qq.com>
2024-06-30 14:24:56 +0800

    删除了EventLoop中的get_epoll方法, 将updata_channel重新添加回来

--- include/EventLoop.hpp
+++ include/EventLoop.hpp
@@ -16,16 +16,6 @@ public:
 
 
     /**
-     * 
-     * @describe: 获取内部的Epoll对象_M_ep
-     * @param:    void
-     * @return:   Epoll*
-     * 
-     */
-    Epoll* get_epoll();
-
-
-    /**
      * @describe: 封装服务器代码中的事件循环过程
      * @param:    void
      * @return:   void
@@ -33,9 +23,17 @@ public:
      */
     void run();
 
+    /**
+     * @describe:  调用成员变量_M_ep的updata_channel
+     * @param:     Channel*
+     * @return:    void
+     */
+    void updata_channel(Channel* ch);
+
 
     ~EventLoop();
 
 private:
+
     Epoll* _M_ep;
 };
--- src/EventLoop.cpp
+++ src/EventLoop.cpp
@@ -6,10 +6,6 @@ EventLoop::EventLoop() : _M_ep(new Epoll){
 }
 
 
-Epoll* EventLoop::get_epoll() {
-    return _M_ep;
-}
-
 void EventLoop::run()
 {
     while(true)
@@ -22,6 +18,10 @@ void EventLoop::run()
     }
 }
 
+void EventLoop::updata_channel(Channel* ch)
+{
+    _M_ep->updata_channel(ch);
+}
 
 EventLoop::~EventLoop() {
     delete _M_ep;
