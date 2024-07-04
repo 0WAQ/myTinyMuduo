db33db87e4acdaa786e0806f51a4b647dd123b32
0WAQ <2758282037@qq.com>
2024-07-04 23:41:59 +0800

    增加ThreadPool类

---
+++ include/ThreadPool.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <sys/syscall.h>
+#include <mutex>
+#include <unistd.h>
+#include <thread>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <atomic>
+
+/**
+ * 
+ * 线程池类
+ * 
+ */
+class ThreadPool
+{
+public:
+
+    /**
+     * 
+     * @describe: 用来初始化线程
+     * @param:    size_t
+     * 
+     */
+    ThreadPool(size_t thread_num);
+
+
+    /**
+     * 
+     * @describe: 将任务添加到任务队列中
+     * @param:    std::function<void()>
+     * @return:   void
+     * 
+     */
+    void push(std::function<void()> task);
+
+
+    ~ThreadPool();
+
+
+private:
+    // 线程池中的线程
+    std::vector<std::thread> _M_threads;
+    // 任务队列
+    std::queue<std::function<void()>> _M_task_queue;
+    // 任务队列同步的互斥锁
+    std::mutex _M_mutex;
+    // 任务队列同步的条件变量
+    std::condition_variable _M_condition;
+    // 在析构函数中, 将其值设置为ture, 全部的线程将退出
+    std::atomic_bool _M_stop;
+};
---
+++ src/ThreadPool.cpp
@@ -0,0 +1,71 @@
+#include "../include/ThreadPool.hpp"
+
+ThreadPool::ThreadPool(size_t thread_num) : _M_stop(false)
+{
+    // 启动thread_num个线程, 将每个线程阻塞在条件变量上
+    for(size_t i = 0; i < thread_num; i++)
+    {   
+        // 利用lambda函数创建线程, 并添加到线程池中
+        _M_threads.emplace_back(
+            [this]() {
+                // 打印进程ID
+                printf("thread(%d) created.\n", syscall(SYS_gettid));
+
+                while(_M_stop == false) {
+                    std::function<void()> task;
+
+                    //////////////////////////////////
+                    {   // 锁作用域开始
+                        std::unique_lock<std::mutex> lock(this->_M_mutex);
+
+                        // 等待生产者的条件变量
+                        this->_M_condition.wait(lock, [this]{
+                            // 只有当等待队列中有任务或者stop时才继续执行
+                            return (this->_M_stop || !this->_M_task_queue.empty());
+                        });
+
+                        // 只有下达stop指令并且没有任务时才能退出
+                        if(this->_M_stop && this->_M_task_queue.empty())
+                            return;
+                        
+                        // 出队
+                        task = std::move(this->_M_task_queue.front()); // 移动语义, 避免拷贝
+                        this->_M_task_queue.pop();
+
+                    }   // 锁作用域结束
+                    //////////////////////////////////
+
+                    printf("thread is %d.\n", syscall(SYS_gettid));
+
+                    // 执行任务
+                    task();
+                }
+            });
+    }
+}
+
+void ThreadPool::push(std::function<void()> task) 
+{
+    ////////////////////////////////////////
+    {   // 锁所用域开始
+
+        std::lock_guard<std::mutex> lock(_M_mutex);
+        _M_task_queue.push(task);
+    
+    }   // 锁作用域结束
+    ////////////////////////////////////////
+
+    // 唤醒一个线程
+    _M_condition.notify_one();
+}
+
+ThreadPool::~ThreadPool() 
+{
+    _M_stop = true;
+    _M_condition.notify_all(); // 唤醒全部线程, 去执行剩余的任务 并且 退出
+
+    // 等待所有线程执行完毕后, 任务退出
+    for(auto &th : _M_threads) {
+        th.join();
+    }
+}
