409a92cf9477a5e49b910a594231f5b86d9bdee3
0WAQ <2758282037@qq.com>
2024-07-21 21:59:01 +0800

    修改Buffer的实现为vector<char>, 并通过测试

--- include/Buffer.hpp
+++ include/Buffer.hpp
@@ -4,6 +4,10 @@
 #include <string>
 #include <cstdint>
 #include <cstring>
+#include <vector>
+#include <unistd.h>
+#include <sys/uio.h>
+#include <cassert>
 
 class Buffer
 {
@@ -11,7 +15,12 @@ public:
     
     /// @brief 初始化buffer
     /// @param sep 选择报文分割类型: 0-无分割符; 1-四字节的报文头; 2-"\r\n\r\n"分割符(Http协议)
-    Buffer(uint16_t sep = 1);
+    Buffer(uint16_t sep = 1, std::size_t prependable_size = 8, std::size_t writable_size = 1024);
+
+
+    /// @brief 将字符串追加到缓冲区, 带有分割符
+    /// @param msg 
+    void append(const std::string& msg);
 
 
     /// @brief 将数据追加至缓冲区
@@ -20,41 +29,68 @@ public:
     void append(const char* data, size_t size);
     
 
-    /// @brief 将带有报头的数据追加至buf
-    /// @param data 同上
-    /// @param size 同上
-    void append_with_sep(const char* data, size_t size);
-
-
     /// @brief 从buf中取出一个报文
     /// @param msg 报文数据
     /// @return 成功true, 失败false
     bool pick_datagram(std::string& msg);
 
 
-    /// @brief 从buf中删除数据
-    /// @param pos 起始位置 
-    /// @param len 长度
-    void erase(size_t pos, size_t len);
-
-
-    /// @brief 获取buf的长度
+    /// @brief 删除size的大小的数据, 并返回
+    /// @param size 
     /// @return 
-    size_t size();
+    std::string erase(std::size_t size);
 
 
-    /// @brief 获取数据首地址
+    /// @brief 扩容
+    /// @param len 扩容到len 
+    void resize(std::size_t len);
+
+
+    /// @brief 确保缓冲区有size的大小
+    /// @param size 
+    void ensure_writable(std::size_t size);
+
+
+    /// @brief 写区域增加len的大小
+    /// @param size 
+    void has_written(std::size_t size) { _M_write_idx += size; }
+
+
+    /// @brief 获取缓冲区的首地址
+    char* begin() { return &*_M_buf.begin(); }
+    const char* cbegin() const { return &*_M_buf.begin(); }
+
+
+    /// @brief 获取缓冲区三个区域的大小
+    /// @return 大小 
+    std::size_t prependable() { return _M_read_idx; }
+    std::size_t readable()    { return _M_write_idx - _M_read_idx; }
+    std::size_t writable()    { return _M_buf.size() - _M_write_idx; }
+
+
+    /// @brief 直接操作内核缓冲区和用户缓冲区
+    /// @param fd 
     /// @return 
-    const char* data();
-    
-    
-    /// @brief 清空buf
-    void clear();
+    std::size_t read_fd(int fd);
+    std::size_t write_fd(int fd);
+
 
 private:
 
-    std::string _M_buf;
+    // prependable的初始大小, 也是read和write idx的初始值
+    std::size_t _M_initial_prependable;
 
+    // writable的初始大小
+    std::size_t _M_initial_writable;
+
+    // read和write区域的索引
+    std::size_t _M_read_idx;
+    std::size_t _M_write_idx;
+
+    // 自动扩容的vector<char>
+    std::vector<char> _M_buf;
+
+    // 分割符
     const uint16_t _M_sep;
     
 };
--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -1,37 +1,36 @@
 #include "../include/Buffer.hpp"
 
-Buffer::Buffer(uint16_t sep) : _M_sep(sep) { }
+Buffer::Buffer(uint16_t sep, std::size_t prependable_size, std::size_t writable_size) : 
+            _M_sep(sep), 
+            _M_initial_prependable(prependable_size), _M_initial_writable(writable_size), 
+            _M_read_idx(_M_initial_prependable), _M_write_idx(_M_initial_prependable),
+            _M_buf(_M_initial_prependable + _M_initial_writable)
+{ }
 
-
-void Buffer::append(const char* data, size_t size) {
-    _M_buf.append(data, size);
-}
-
-void Buffer::append_with_sep(const char* data, size_t size)
+void Buffer::append(const std::string& msg)
 {
     switch (_M_sep)
     {
         case 0: // 无分割符
         {
-            _M_buf.append(data, size);
-
+            append(msg.data(), msg.size());
             break;
         }
         case 1: // 固定长度4字节做报文头
         {
             // 先将数据的大小送入缓冲区
-            _M_buf.append((char*)&size, 4);
+            size_t len = msg.size();
+            append((char*)&len, 4);    
             // 再将数据本身送入缓冲区
-            _M_buf.append(data, size);
-
+            append(msg.data(), msg.size());
             break;
         }
         case 2: // 报文尾部加"\r\n\r\n"
         {
             // 先将数据放入缓冲区
-            _M_buf.append(data, size);
+            append(msg.data(), msg.size());
             // 再放入分割符
-            _M_buf.append("\r\n\r\n", 4);
+            append("\r\n\r\n", 4);
 
             break;
         }
@@ -39,46 +38,80 @@ void Buffer::append_with_sep(const char* data, size_t size)
 
 }
 
+void Buffer::append(const char* data, std::size_t size)
+{
+    assert(data != nullptr);
+    ensure_writable(size);
+    std::copy(data, data + size, begin() + _M_write_idx);
+    has_written(size);
+}
+
+std::string Buffer::erase(std::size_t size)
+{
+    if(size > readable()) {
+        std::cout << "删除失败" << std::endl; 
+        return {};
+    }
+
+    std::string res(begin() + _M_read_idx, size);
+
+    // 将索引向后移动
+    _M_read_idx += size;
+
+    // 若移动后, payload大小为0, 则重置索引
+    if(readable() == 0) {
+        _M_read_idx = _M_write_idx = _M_initial_prependable;
+    }
+
+    return res;
+}
+
 bool Buffer::pick_datagram(std::string& msg)
 {
-    if(_M_buf.empty())  
+    // 若可读区域大小为0, 失败
+    if(readable() == 0)
         return false;
 
     switch (_M_sep)
     {
         case 0:
         {
-            msg = _M_buf;
-            _M_buf.clear();
-
+            msg = std::string{begin() + _M_read_idx, begin() + _M_write_idx};
+            erase(readable());
             break;
         }
         case 1:
         {
-            int len; // 获取报文头部
-            memcpy(&len, _M_buf.data(), 4); // 从buf中获取报头
-            
-            // 若接收缓冲区中的数据量长度小于报文(有分包现象), 则不完整, 等待
-            if(_M_buf.size() < len + 4)
+            std::size_t len = 0; // 获取报文头部的长度
+            memcpy(&len, _M_buf.data() + _M_read_idx, 4);
+
+            // 若payload的长度小于报文(有分包现象), 则不完整, 等待
+            if(readable() < len + 4)
                 return false;
 
             // 跳过报文头, 只取报文内容
-            msg.append(_M_buf.data() + 4, len);
-            _M_buf.erase(0, len + 4); // 将该报文从缓冲区中删除
+            msg = {begin() + _M_read_idx + 4, len};
+            erase(len + 4); // 将该报文从缓冲区中删除
 
             break;
         }
         case 2:
         {
-            int len = _M_buf.find("\r\n\r\n");
+            std::size_t len = std::string::npos;
+            for(std::size_t i = _M_read_idx; i + 3 < _M_write_idx; i++) {
+                if(std::string{begin() + i, begin() + i + 4} == "\r\n\r\n") {
+                    len = i;
+                    break;
+                }
+            }
 
             // 没有找到分割符 或者 报文不完整
-            if(len == std::string::npos || _M_buf.size() < len + 4)
+            if(len == std::string::npos || readable() < len - _M_read_idx + 4)
                 return false;
 
             // 将报文放到msg中
-            msg.append(_M_buf.data(), len);
-            _M_buf.erase(0, len + 4);
+            msg = {begin() + _M_read_idx, begin() + len};
+            erase(len - _M_read_idx + 4);
 
             break;
         }
@@ -87,18 +120,43 @@ bool Buffer::pick_datagram(std::string& msg)
     return true;
 }
 
-void Buffer::erase(size_t pos, size_t len) { 
-    _M_buf.erase(pos, len);
+void Buffer::ensure_writable(std::size_t size)
+{
+    // 确保可写区域有size的大小, 否则扩容
+    if(writable() < size) {
+        resize(size);
+    }
 }
 
-std::size_t Buffer::size() { 
-    return _M_buf.size();
+void Buffer::resize(std::size_t len)
+{
+    // 当writable和读空下来的prependable不够时, 重新分配
+    if(writable() + prependable() < len + _M_initial_prependable) 
+    {
+        // 重新分配空间
+        _M_buf.resize(_M_write_idx + len);
+    }
+    else // 空下来的大小足够分配
+    {
+        // 提前获取payload的大小
+        std::size_t readable_bytes = readable();
+        
+        // 将payload移动到初始位置
+        std::copy(begin() + _M_read_idx, begin() + _M_write_idx, 
+                  begin() + _M_initial_prependable);
+
+        // 更新两个索引的位置        
+        _M_read_idx = _M_initial_prependable;
+        _M_write_idx = _M_read_idx + readable_bytes;
+    }
 }
 
-const char* Buffer::data() { 
-    return _M_buf.data();
+std::size_t Buffer::read_fd(int fd)
+{
+
 }
 
-void Buffer::clear() { 
-    _M_buf.clear();
+std::size_t Buffer::write_fd(int fd)
+{
+    
 }
