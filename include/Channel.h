21a80ad67c42551ec34ca1c686b052da32720b81
0WAQ <2758282037@qq.com>
2025-02-23 15:43:12 +0800

    修改部分逻辑, 新增tie, index, handle_with_guard

--- include/Channel.h
+++ include/Channel.h
@@ -6,104 +6,131 @@
 #ifndef CHANNEL_H
 #define CHANNEL_H
 
-#include <sys/epoll.h>
 #include <functional>
+#include <memory>
+#include "noncopyable.h"
 #include "EventLoop.h"
 #include "Socket.h"
+#include "TimeStamp.h"
 
 class Socket;
 class EventLoop;
+class TimeStamp;
 
 /**
- * 
- * 包含了一个EventLoop*的成员变量(封装了一个Epoll对象)
- * 封装了让Epoll对象监视Socket对象所需的各种变量
+ * 封装了sockfd感兴趣的event, 还绑定了poller返回的具体事节
  */
-class Channel
+class Channel : noncopyable
 {
 public:
 
-    using ReadCallback = std::function<void()>;
-    using WriteCallback = std::function<void()>;
-    using CloseCallback = std::function<void()>;
-    using ErrorCallback = std::function<void()>;
+    using EventCallbacl = std::function<void()>;
+    using ReadCallback = std::function<void(TimeStamp)>;
 
 public:
 
-
-    /// @brief 将fd与事件循环绑定
-    /// @param loop_ptr 事件循环
-    /// @param fd fd
+    /**
+     * @brief 将fd与事件循环绑定
+     * @param loop_ptr 事件循环
+     * @param fd fd
+     */
     Channel(EventLoop* loop_ptr, int fd);
 
+    /**
+     * @brief 事件发生后的处理函数, 会回调事件的函数
+     */
+    void handle(TimeStamp receiveTime);
 
-    /// @brief 事件发生后的处理函数
-    void handle();
-
-
-    /// @brief 四种事件
+    /**
+     * @brief 四种事件
+     */
     void set_read_events();
     void set_write_events();
     void unset_read_events();
     void unset_write_events();
     void unset_all_events();
 
-
-
-    /// @brief 从事件循环中删除Channel
+    /**
+     * @brief 从事件循环中删除Channel
+     */
     void remove();
 
-
-    /// @brief 用来设置和判断该channel是否已被监视
+    /**
+     * @brief 用来设置和判断该channel是否已被监视
+     */
     void set_in_epoll();
     bool in_epoll();
 
-
-    /// @brief 设置边缘触发
+    /**
+     * @brief 设置边缘触发
+     */
     void set_ET();
 
-
-    /// @brief 设置, 获取事件
-    /// @param events 
+    /**
+     * @brief 设置, 获取事件
+     * @param events 
+     */
     void set_happened_events(uint32_t events);
     uint32_t get_happened_events();
     uint32_t get_monitored_events();
 
+    /**
+     * @brief 设置回调函数
+     * @param cb 函数对象
+     */
+    void set_read_callback(ReadCallback cb);   // 读事件
+    void set_write_callback(EventCallbacl cb); // 写事件
+    void set_close_callback(EventCallbacl cb); // 关闭连接
+    void set_error_callback(EventCallbacl cb); // 错误事件
 
+    bool is_none_events() { return _M_monitored_events == _M_none_events; }
+    bool is_reading() { return _M_monitored_events == _M_read_events; }
+    bool is_writin() { return _M_monitored_events == _M_write_events; }
 
-    /// @brief 设置回调函数
-    /// @param func 函数对象
-    void set_read_callback(ReadCallback func);  // 读事件
-    void set_write_callback(WriteCallback func); // 写事件
-    void set_close_callback(CloseCallback func); // 关闭连接
-    void set_error_callback(ErrorCallback func); // 错误事件
+    /**
+     * @brief 防止channel被remove后, channel还在执行回调操作 TODO:
+     * @param 
+     */
+    void tie(const std::shared_ptr<void>&);
 
+    int  get_index() { return _M_index; }
+    void set_index(int idx) { _M_index = idx; }
 
-    /// @brief 获取channel的fd
-    /// @return fd
     int get_fd();
-
+    EventLoop* owner_loop() { return _M_loop_ptr; }
 
 private:
-    // channel两边的fd和loop
-    const int _M_fd = -1;
-    EventLoop* _M_loop_ptr = nullptr;
+
+    void update();
+    void handle_event_with_guard(TimeStamp receiveTime);
+
+    static const int _M_read_events = EPOLLIN | EPOLLPRI;
+    static const int _M_write_events = EPOLLOUT;
+    static const int _M_none_events = 0;
+
+    const int _M_fd = -1;               // poller监听的对象
+    EventLoop* _M_loop_ptr = nullptr;   // 事件循环
     
     bool _M_in_epoll = false;
     uint32_t _M_monitored_events = 0;
     uint32_t _M_happened_events = 0;
+    uint32_t _M_index;                  // TODO:
+
+    // TODO: 与shared_from_this?
+    std::weak_ptr<void> _M_tie;         // 
+    bool _M_tied;
 
     // 读事件的回调函数, 将回调Acceptor::new_connection或者new_message
     ReadCallback _M_read_callback; 
 
     // 写事件的回调函数, 将回调Connection::send
-    WriteCallback _M_write_callback;
+    EventCallbacl _M_write_callback;
 
     // 连接关闭的回调函数, 将回调Connection::close_callback
-    CloseCallback _M_close_callback; 
+    EventCallbacl _M_close_callback; 
 
     // 连接出错的回调函数, 将回调Connection::error_callback
-    ErrorCallback _M_error_callback; 
+    EventCallbacl _M_error_callback; 
 };
 
 #endif // CHANNEL_H
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,59 +1,100 @@
 #include "Channel.h"
 #include "Connection.h"
+#include "Logger.h"
 
 Channel::Channel(EventLoop* loop_ptr, int fd) : 
-        _M_loop_ptr(loop_ptr), _M_fd(fd) 
+        _M_loop_ptr(loop_ptr),
+        _M_fd(fd),
+        _M_monitored_events(0),
+        _M_happened_events(0),
+        _M_index(-1),
+        _M_tied(false)
 { }
 
 // 判断该fd对应的channel发生的是什么事件
-void Channel::handle()
-{   
+void Channel::handle(TimeStamp receiveTime)
+{
+    std::shared_ptr<void> guard;
+    if(_M_tied) {
+        guard = _M_tie.lock();
+        if(guard) {
+            handle_event_with_guard(receiveTime);
+        }
+    }
+    else {
+        handle_event_with_guard(receiveTime);
+    }
+}
+
+void Channel::handle_event_with_guard(TimeStamp receiveTime)
+{
+    LOG_INFO("channel handle happened events: %d\n", _M_happened_events);
+
     // 连接中断事件
-    if(_M_happened_events & EPOLLRDHUP) 
-        _M_close_callback();
+    if(_M_happened_events & EPOLLRDHUP) {
+        if(_M_close_callback) {
+            _M_close_callback();
+        }
+    }
 
     // 读事件
-    else if(_M_happened_events & (EPOLLIN | EPOLLPRI)) 
-        _M_read_callback();
+    if(_M_happened_events & _M_read_events) {
+        if(_M_read_callback) {
+            _M_read_callback(receiveTime);
+        }
+    }
 
     // 写事件
-    else if(_M_happened_events & EPOLLOUT) 
-        _M_write_callback();
+    if(_M_happened_events & _M_write_events) {
+        if(_M_write_callback) {
+            _M_write_callback();
+        }
+    }
 
     // 错误
-    else 
-        _M_error_callback(); 
-    
+    if(_M_happened_events & EPOLLERR) {
+        if(_M_error_callback) {
+            _M_error_callback(); 
+        }
+    }
 }
 
-// 取消对该fd的监视
-void Channel::remove()
-{
-    unset_all_events();         // 先取消全部的事件
-    _M_loop_ptr->remove_channel(this); // 从红黑树上删除fd
+void Channel::remove() {
+    unset_all_events();
+    _M_loop_ptr->remove_channel(this);
 }
+
+void Channel::update() {
+    _M_loop_ptr->update_channel(this);
+}
+
 
 /// 获取于设置内部成员变量
-int Channel::get_fd() { return _M_fd;}
-bool Channel::in_epoll() {return _M_in_epoll;}
-void Channel::set_in_epoll() {_M_in_epoll = true;}
-void Channel::set_happened_events(uint32_t events) {_M_happened_events = events;}
-uint32_t Channel::get_happened_events() {return _M_happened_events;}
-uint32_t Channel::get_monitored_events() {return _M_monitored_events;}
+int Channel::get_fd() { return _M_fd; }
+bool Channel::in_epoll() { return _M_in_epoll; }
+void Channel::set_in_epoll() { _M_in_epoll = true; }
+void Channel::set_happened_events(uint32_t events) { _M_happened_events = events; }
+uint32_t Channel::get_happened_events() { return _M_happened_events; }
+uint32_t Channel::get_monitored_events() { return _M_monitored_events; }
+
+// TODO: 什么时候调用
+void Channel::tie(const std::shared_ptr<void>& obj) {
+    _M_tie = obj;
+    _M_tied = true;
+}
 
 // 设置边缘触发
 void Channel::set_ET() { _M_monitored_events |= EPOLLET;}
 
 // 设置监听事件
-#define updata _M_loop_ptr->updata_channel(this)
-void Channel::set_read_events() {    _M_monitored_events |= EPOLLIN;   updata;}
-void Channel::set_write_events() {   _M_monitored_events |= EPOLLOUT;  updata;}
-void Channel::unset_read_events() {  _M_monitored_events &= ~EPOLLIN;  updata;}
-void Channel::unset_write_events() { _M_monitored_events &= ~EPOLLOUT; updata;} 
-void Channel::unset_all_events() {   _M_monitored_events = 0;          updata;}
+void Channel::set_read_events() {    _M_monitored_events |= _M_read_events;   update(); }
+void Channel::set_write_events() {   _M_monitored_events |= _M_write_events;  update(); }
+void Channel::unset_read_events() {  _M_monitored_events &= ~_M_read_events;  update(); }
+void Channel::unset_write_events() { _M_monitored_events &= ~_M_write_events; update(); } 
+void Channel::unset_all_events() {   _M_monitored_events = _M_none_events;    update(); }
 
 // 读,写,关闭,错误 四个设置回调函数
-void Channel::set_read_callback (ReadCallback func) {_M_read_callback  = std::move(func);}
-void Channel::set_write_callback(WriteCallback func) {_M_write_callback = std::move(func);}
-void Channel::set_close_callback(CloseCallback func) {_M_close_callback = std::move(func);}
-void Channel::set_error_callback(ErrorCallback func) {_M_error_callback = std::move(func);}
+void Channel::set_read_callback (ReadCallback cb)  { _M_read_callback  = std::move(cb); }
+void Channel::set_write_callback(EventCallbacl cb) { _M_write_callback = std::move(cb); }
+void Channel::set_close_callback(EventCallbacl cb) { _M_close_callback = std::move(cb); }
+void Channel::set_error_callback(EventCallbacl cb) { _M_error_callback = std::move(cb); }
