88dc0190dec44ce7472a71915106c150b8eca891
0WAQ <2758282037@qq.com>
2025-02-23 14:28:07 +0800

    InetAddress

--- include/InetAddress.h
+++ include/InetAddress.h
@@ -9,35 +9,31 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <string>
-
+#include <strings.h>
 
 class InetAddress
 {
 public:
+    
+    InetAddress() = default;
 
+    /**
+     * @brief 用ip和port初始化sockaddr_in的内部变量 
+     * @param port 接收主机序的port
+     */
+    explicit InetAddress(const std::string& ip, uint16_t port);
 
-    InetAddress();
+    explicit InetAddress(const sockaddr_in addr) : _M_addr(addr) { }
 
+    std::string get_ip() const;
 
-    /// @brief 用ip和port初始化sockaddr_in的内部变量 
-    /// @param ip string的cv类型
-    /// @param port 接收主机序的port
-    InetAddress(const std::string& ip, uint16_t port);
-
-
-    /// @brief 拷贝构造
-    /// @param addr 
-    InetAddress(const sockaddr_in addr);
-
-
-    /// @brief 返回ip, port, addr
-    const char* get_ip() const;
     uint16_t get_port() const;
+
     const sockaddr* get_addr() const;
 
-
-    /// @brief 服务端使用accept接收连接后, 将填充的sockaddr_in结构体设置给InetAddress类
-    /// @param clnt_addr 客户端地址结构体
+    /**
+     * @brief 服务端使用accept接收连接后, 将填充好的sockaddr_in结构体设置给InetAddress
+     */
     void set_addr(sockaddr_in clnt_addr);
 
 private:
--- src/InetAddress.cpp
+++ src/InetAddress.cpp
@@ -1,20 +1,24 @@
 #include "InetAddress.h"
 
-InetAddress::InetAddress() { }
-
-InetAddress::InetAddress(const std::string& _ip, uint16_t _port)
-{
+InetAddress::InetAddress(const std::string& ip, uint16_t port) {
+    bzero(&_M_addr, sizeof(sockaddr_in));
     _M_addr.sin_family = AF_INET;
-    _M_addr.sin_addr.s_addr = inet_addr(_ip.c_str());
-    _M_addr.sin_port = htons(_port);
+    _M_addr.sin_addr.s_addr = inet_addr(ip.c_str());
+    _M_addr.sin_port = htons(port);
 }
 
-InetAddress::InetAddress(const sockaddr_in _addr):_M_addr(_addr) { }
+std::string InetAddress::get_ip() const {
+    return ::inet_ntoa(_M_addr.sin_addr);
+}
 
-const char* InetAddress::get_ip() const {return inet_ntoa(_M_addr.sin_addr);}
+uint16_t InetAddress::get_port() const {
+    return ntohs(_M_addr.sin_port);
+}
 
-uint16_t InetAddress::get_port() const {return ntohs(_M_addr.sin_port);}
+const sockaddr* InetAddress::get_addr() const { 
+    return (sockaddr*)&_M_addr;
+}
 
-const sockaddr* InetAddress::get_addr() const {return (sockaddr*)&_M_addr;}
-
-void InetAddress::set_addr(sockaddr_in clnt_addr) {_M_addr = clnt_addr;}
+void InetAddress::set_addr(sockaddr_in clnt_addr) {
+    _M_addr = clnt_addr;
+}
